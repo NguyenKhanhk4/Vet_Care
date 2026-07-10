9ca873a11b29707db47c1a9c01e375a5870f7bb6
VuViet06 <vudinhviet162@gmail.com>
2026-07-10 15:57:24 +0700

    update code

--- mobile/src/shared/utils/api.ts
+++ mobile/src/shared/utils/api.ts
@@ -21,10 +21,7 @@ const api = axios.create({
   },
 });
 
-/**
- * Request Interceptor
- * Automatically attaches JWT token to every request
- */
+
 api.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
     try {
